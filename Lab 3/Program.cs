ce6753c248b3942b7ccd6a3ffe673b38d224a8ef
Ruslan Yarema <yarema2003r@gmail.com>
2022-12-19 15:26:49 +0200

    Quests added

--- Lab 3/Program.cs
+++ Lab 3/Program.cs
@@ -12,32 +12,51 @@ namespace Program
         {
             Customer person = new Customer("John Balboa", 22);
             Manager manager = new Manager("Micky Green", 31);
-
-            manager.PickOption();
-
-            var punct = Convert.ToInt32(Console.ReadLine());
-            switch (punct)
+            Assistant assistant = new Assistant("Max Troy", 21);  
+            do
             {
-                case 0:
-                    Computer.ViewTours();
-                    break;
-                case 1:
-                    Computer.buyTickets();
-                    break;
-                case 2:
-                    Computer.myTickets();
-                    break;
-                case 3:
-/*                    Computer.changeManager();
-*/                    break;
-                case 4:
-                    Computer.AboutAgency();
-                    break;
-                default:
-                    Console.WriteLine("It's imposible!");//це неможливо
-                    return;
+                
+
+                manager.PickOption();
+
+                var menu_punct = Convert.ToInt32(Console.ReadLine());
+                switch (menu_punct)
+                {
+                    case 0:
+                        Computer.ViewTours(ref manager, menu_punct);
+                        break;
+                    case 1:
+                        Computer.buyTickets(ref manager, menu_punct);
+                        break;
+                    case 2:
+                        Computer.myTickets(ref manager, menu_punct);
+                        break;
+                    case 3:
+                        Computer.changeManager(ref manager, menu_punct);
+                        break;
+                    case 4:
+                        Computer.ChooseAssistant(ref assistant, ref manager, menu_punct);
+                        break;
+                    case 5:
+                        Bag.MoneyCount();
+                        break;
+                    case 6:
+                        Computer.AboutAgency(ref manager, menu_punct);
+                        break;
+                    case 7:
+                        manager.Talk(ref manager, menu_punct);
+                        break;
+                    case 10:
+                        System.Environment.Exit(-1);
+                        break;
+                    default:
+                        Console.WriteLine("It's imposible!");//це неможливо
+                        return;
+
+                }
 
             }
+            while (true);
         }
     }
     public abstract class Person
@@ -59,53 +78,58 @@ namespace Program
             { "", "", "" }
 
         };
+        public static int[] TicketsCount { get; set; } = new int[mytickets.GetUpperBound(0) + 1];
+
+
         public Customer(string name, int age) : base(name, age)
         {
 
         }
-        
+
     }
-    public  class Computer
+    public class Computer
     {
         public static string[] prefix = new string[] { "Country:", "Arrival date:", "Cost($):" };
-        public static object[,] tours = new object[,] 
-        { 
-            { "Jamaica", "01.10.2022", "500"}, 
-            { "Vienna", "20.12.2022", "250" },
-            { "Changhai", "14.12.2022", "350" }
+        public static object[,] tours = new object[,]
+        {
+            { "Alps", "01.10.2022", "500"},
+            { "Norway", "20.12.2022", "250" },
+            { "Sahara desert", "14.12.2022", "350" }
 
         };
-        public static void ViewTours()
+        public static void ViewTours(ref Manager manager, int menu_punct)
         {
-            for(int y = 0; y <= 2; y++) 
+            manager.Talk(ref manager, menu_punct);
+            for (int y = 0; y <= 2; y++)
             {
-            for (int i = 0; i < tours.GetUpperBound(0) + 1 ; i++)
-            {
+                for (int i = 0; i < tours.GetUpperBound(0) + 1 ; i++)
+                {
 
-                Console.WriteLine(prefix[i]);
-                Console.WriteLine(tours[y, i]);
-            }
+                    Console.WriteLine(prefix[i]);
+                    Console.WriteLine(tours[y, i]);
+                }
                 Console.WriteLine("\n");
 
             }
-            
-        }   
-        public static void buyTickets()
+        }
+        public static void buyTickets(ref Manager manager , int menu_punct)
         {
-            Computer.ViewTours();
+            Computer.ViewTours(ref manager, menu_punct);
             Console.WriteLine("Choose tour, you want to buy tickets to.");
             var punct = Convert.ToInt32(Console.ReadLine());
+            manager.Talk(ref manager, menu_punct);
+            menu_punct = 8;
             switch (punct)
             {
                 case 0:
                     buyTickets(0);
                     Console.WriteLine("Succesfully buyed");
-                    
+
                     break;
                 case 1:
                     buyTickets(1);
                     Console.WriteLine("Succesfully buyed");
-                    
+
                     break;
                 case 2:
                     buyTickets(2);
@@ -116,71 +140,77 @@ namespace Program
                     return;
 
             }
-            Program.Main();
+
         }
 
         public static void buyTickets(int i)
         {
             for (int x = 0; x < 1; x++)
-            { 
+            {
                 for (int y = 0; y < tours.GetUpperBound(0) + 1; y++)
                 {
-                    if (Convert.ToString(Customer.mytickets[x,y]) != "")
+                    if (Convert.ToString(Customer.mytickets[x, y]) != "")
                     {
                         x++;
-                        Customer.mytickets[x, y] = tours[i, y];
                     }
-                    else
-                    {
-                        Customer.mytickets[x, y] = tours[i, y];
-
-                    }
+                    Customer.mytickets[x, y] = tours[i, y];
+                    Customer.TicketsCount[x]++;
                 }
             }
         }
-        public static void myTickets()
+        public static void myTickets(ref Manager manager, int menu_punct)
         {
+            manager.Talk(ref manager, menu_punct);
+
             for (int y = 0; y <= 2; y++)
             {
-                for (int i = 0; i <= 2; i++)
+                for (int i = 0; i < Customer.TicketsCount[y]; i++)
                 {
 
-                    Console.WriteLine(prefix[i]);
+                    Console.Write(prefix[i]);
                     Console.WriteLine(Customer.mytickets[y, i]);
                 }
                 Console.WriteLine("\n");
 
             }
-            Program.Main();
-        }
-        /*public static void changeManager()
-        {
-            Manager manager = new Manager("John Cat", 31);
 
+        }
+        public static void changeManager(ref Manager manager, int menu_punct) 
+        {
+
+
+            manager.Talk(ref manager, menu_punct);
 
             if (Convert.ToString(manager._name) != "John Cat")
             {
-                Manager manager = new Manager("John Cat", 31);
+                manager = new Manager("John Cat", 31);
                 Console.WriteLine("Hi, i'm " + manager._name + ", my age is " + manager._age + ", and i will be your new manager.");
-                Program.Main();
+                
             }
             else
             {
-                Manager manager = new Manager("Micky Green", 31);
+                manager = new Manager("Micky Green", 31);
                 Console.WriteLine("Hi, i'm " + manager._name + ", my age is " + manager._age + ", and i will be your new manager.");
-                Program.Main();
+                
             }
-        }*/
-        public static void AboutAgency()
+        }
+        public static void ChooseAssistant(ref Assistant assistant,ref Manager manager ,int menu_punct)
         {
+            manager.Talk(ref manager, menu_punct);
+            Console.WriteLine("Hi, im " + assistant._name + ", my age is " + assistant._age + ", and i will be your assistant");
+        }
+        public static void AboutAgency(ref Manager manager, int punct)
+        {
+            manager.Talk(ref manager, punct);
             Console.WriteLine("Our agensy is cool");
-            Program.Main();
+
         }
     }
     public class Manager : Person
     {
         public string[] questions = new string[] {"\nChoose a punct:", "0. View current tours", "1. Buy tickets",
-                    "2. My tickets", "3. Change manager", "4. About our agency\n" };
+                    "2. My tickets", "3. Change manager", "4. Choose assistant", "5. My money", "6. About our agency" };
+        public string[] phrases = new string[] {"Here is our current tours: \n", "You can buy tickets for this tours: \n", "Here is your tickets: \n", "Ok, i will ask another manager.\n", "Here is your assistant: \n", "", "About our agency: \n", "" };
         public Manager(string name, int age) : base(name, age)
         {
 
@@ -188,11 +218,67 @@ namespace Program
 
         public void PickOption()
         {
-            for (int i=0 ;i < questions.Length ; i++)
+            for (int i = 0; i < questions.Length; i++)
             {
                 Console.WriteLine(questions[i]);
             }
         }
-        
+        public void Talk(ref Manager manager, int punct)
+        {
+            Console.WriteLine(manager._name + ":\n" + phrases[punct]);
+            if(punct == 10)
+            {
+                Console.WriteLine(manager._name + ":\n");
+
+            }
+        }
+
     }
+    public class Assistant : Person
+    {
+        public Assistant(string name, int age) : base(name, age)
+        {
+
+        }
+    }
+    public class Bag
+    {
+
+        public class Money
+        {
+        public static int _count = 500;
+        }
+
+        public class Items
+        {
+            public string[] items = new string[] { };
+        }
+        public static void MoneyCount()
+        {
+            Console.WriteLine("Your money: " + Bag.Money._count + "$");
+        }
+    }
+    public class Country
+    {
+
+    }
+    public class AR
+    {
+
+    }
+    public class Quest
+    {
+        public string[] items = new string[] {"Alpine flower", "Catfish teeth", "Cammel wool"};
+
+    }
+    /*Ideas:
+     * change program conception to adaptive reality tour agency
+     * add each country own class(Jamaica, Vienna, Changhai), where all quests contains
+     * add opportunity to enter to chosen country and do some quests
+     * add opportunity to collect some items from quests
+     * create a bag, where you can see your collected items from tours
+     the game will be finished, when all items are collected 
+     add assistante class an hiwm function
+     add apartive reality system 
+    */
 }
